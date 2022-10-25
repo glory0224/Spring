7f2382164185a8d28065c5e02c5ddc18362508eb
twozeropang <pma5758@naver.com>
2022-10-25 09:59:07 +0900

    MVC1 Model Pattern Study

--- src/main/java/glory/spring/web/board/BoardVO.java
+++ src/main/java/glory/spring/web/board/BoardVO.java
@@ -20,8 +20,8 @@ public class BoardVO {
 	public String getTitle() {
 		return title;
 	}
-	public void setTitle(String titile) {
-		this.title = titile;
+	public void setTitle(String title) {
+		this.title = title;
 	}
 	public String getWriter() {
 		return writer;
--- src/main/java/glory/spring/web/board/impl/BoardDAO.java
+++ src/main/java/glory/spring/web/board/impl/BoardDAO.java
@@ -89,6 +89,8 @@ public class BoardDAO {
 			board = new BoardVO();
 			board.setSeq(rs.getInt("seq"));
 			board.setTitle(rs.getString("title"));
+			board.setWriter(rs.getString("writer"));
+			board.setContent(rs.getString("content"));
 			board.setRegDate(rs.getDate("regdate"));
 			board.setCnt(rs.getInt("cnt"));
 		}
